1edf329033a7926105e735cb4b6a10ae692367df
MessineB <messinebelarousi@gmail.com>
2021-12-30 15:22:34 +0100

    il manque la validation

--- JS.js
+++ JS.js
@@ -9,7 +9,6 @@ const getJson = async() => {
     return data.QUESTIONS;
 }
 
-
 function shuffleArray(array) {                         // Fonction du tri aleatoir de ses morts
     array.sort((a, b) => 0.5 - Math.random());          
 
@@ -18,17 +17,38 @@ function shuffleArray(array) {                         // Fonction du tri aleato
  async function ReadQuestion () {     
     const MyData = await getJson();             //Je recupere mes données Json et je les envoi dans un tableau
     Tableau.push(...MyData);                    //                       
-    console.log(Tableau)
-
+    //console.log(Tableau)
 }
-
 
 function AfficheQuestion() {
-    console.log(Tableau)
-    var question1 = Tableau[0].question;                  //   J'affiche la premiere question de mon tableau dans mon html
-    var text1 = document.getElementById('1quest');      //
-    text1.innerHTML = question1;           
+
+    var Main = document.getElementsByClassName("quizz")
+
+    for ( var u=0 ; u<9 ; u++) {
+        let form = document.createElement("form")
+        form.className="Formulaire"
+        let Paragraph = document.createElement("p")
+        form.appendChild(Paragraph)
+        Paragraph.innerHTML = Tableau[u].question;
+
+
+        for ( var i=0 ; i < 4 ; i++) {
+            let Input = document.createElement("input")
+            Input.setAttribute("type", "radio")
+            Input.name = "Input"
+            Input.value = Tableau[u].propositions[i]
+            let Label = document.createElement("label")
+            Label.innerHTML = Tableau[u].propositions[i]
+            form.appendChild(Input)
+            form.appendChild(Label)
+            console.log(Label)
+    }
+    Main[0].appendChild(form)
+
+    }
 }
+
+
 
 async function init() {
     await ReadQuestion()
@@ -36,8 +56,3 @@ async function init() {
 }
 
 init()
-
-
-
- 
- 
