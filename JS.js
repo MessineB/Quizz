a57d2cfde2e605bfd8e05c87973ff957d00234af
MessineB <messinebelarousi@gmail.com>
2022-01-19 12:13:33 +0100

    local storage

--- JS.js
+++ JS.js
@@ -1,7 +1,8 @@
 
 const api_url ='https://messineb.github.io/JASON.Json'
 const Tableau = []
-const Nbrquestion = 9
+const Tableauscore = []
+const Nbrquestion = 1
 
 const getJson = async() => {  
     const response = await fetch(api_url);
@@ -26,74 +27,81 @@ function AfficheQuestion() {                    //La fonction qui affiche les qu
     var Main = document.getElementsByClassName("quizz") //Je recupere mon main dans une variable Quizz
 
     for ( var u=0 ; u< Nbrquestion ; u++) {     //Un boucle qui crée des Form, j'en crée *Nbrquestion
-        let form = document.createElement("form")   //Je crée un form
+        var form = document.createElement("form")   //Je crée un form
         form.className="Formulaire"                 // Je lui donne une classe = Formulaire
-        let Paragraph = document.createElement("p") //Une balise text dans laquelle j'affiche la question
+        var Paragraph = document.createElement("p") //Une balise text dans laquelle j'affiche la question
         form.appendChild(Paragraph)                 //
         Paragraph.innerHTML = Tableau[u].question;  //
 
         for ( var i=0 ; i < Tableau[u].propositions.length ; i++) { //Dans chaque question je crée des propositions
-            let Input = document.createElement("input")   //On recupere chaque input separement que l'on place dans une variable input
+            var Input = document.createElement("input")   //On recupere chaque input separement que l'on place dans une variable input
             Input.type="radio"     //J'attribue a mes input un type et je met en radio
             Input.name = "Input"    //J'attribue un name a mes input = Input
             Input.value = Tableau[u].propositions[i]    //On attribue une value a mes input qui me permettera de recuperé la valeur plus tard 
-            let Label = document.createElement("label") //On crée des label en HTML et on le relie avec une variable Label en JS
+            var Label = document.createElement("label") //On crée des label en HTML et on le relie avec une variable Label en JS
             Label.innerHTML = Tableau[u].propositions[i] //Dans mon Label je met les propositions
             form.appendChild(Input)   
-            form.appendChild(Label)
-            
+            form.appendChild(Label)           
     }
     Main[0].appendChild(form)
     }
 }
 
-
-
 const BoutonJS = document.getElementById('bouton')
-
+const BoutonJS2 = document.getElementById('bouton2')
 async function init() {
     await ReadQuestion();
     AfficheQuestion();
-    Joueur();
     BoutonJS.addEventListener("click" , Verify) 
-}
+    BoutonJS2.addEventListener("click",effacestorage)
+    
 
+}
 init()
-function Joueur() {
-    var Nomdujoueur = prompt("Bienvenue, entre ton nom joueur !")
-   document.getElementById('articleHeader').innerText = "Bienvenue sur mon quizz " + Nomdujoueur  
-}
-
-var Score=0
 
 
+var Score=0;
 
 function Verify() {
+    var debut = document.getElementById("debut");
     var Check = [];
     Check = document.querySelectorAll("input:checked");
-    
     if ( Check.length == Nbrquestion){
-    for (let v=0 ; v<9 ; v++) {
+    for (let v=0 ; v<1 ; v++) {
         if ( Check[v].value === Tableau[v].réponse ) {
-            Score++
-            
-           
+            Score++   
         }
-        else {
-            myFunction()
-        }
-        
     }
     document.getElementById("ScoreFinal").innerText = "Votre score est de : " + Score  
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+    var Nomdujoueur = prompt("Bienvenue, entre ton nom joueur !")
+document.getElementById('articleHeader').innerText = "Merci d'avoir joué a mon quizz " + Nomdujoueur  
+  localStorage.setItem(Nomdujoueur , Score);
+    stockage = localStorage;
+    console.log(stockage)
+    for ( let s=0; s<5 ;s++ ) {
+    var best = stockage.key(s);
+    bestscore = localStorage.getItem(best);
+    var affichescore = document.createElement("article");
+    const variar = document.createTextNode(best)
+    const variar2 = document.createTextNode(bestscore)
+    affichescore.appendChild(variar)
+    affichescore.appendChild(variar2)
+    debut.appendChild(affichescore);
+    affichescore.innerHTML = " Numero "+s+" est " + best +" avec un score de : "  +bestscore ;
+}
 }
     else {
     alert("Vous devez remplir toutes les questions !")
     
 }
+
     Score=0
 }
 
+function effacestorage(stockage) {
+    localStorage.clear(stockage);
+}
 
 
